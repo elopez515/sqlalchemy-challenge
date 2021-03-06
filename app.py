884321c274bb99c54c74cffd5b6627eb06620907
Efrain Lopez <e.lopez515@outlook.com>
2021-03-06 15:42:24 -0800

    Set up our route allowing user to get back the min, avg, & max temperature observation for their imputed date or date range

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ def Welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/yyyy-mm-dd/yyyy-mm-dd"
     )
 
 # Set up our route for the precipitation data
@@ -56,6 +57,7 @@ def precipitation():
     prcp = {date: prcp for date, prcp in prcp_df}
     return jsonify(prcp)
 
+# Set up our route for the station names
 @app.route("/api/v1.0/stations")
 def stations():
     # Create our session (link) from Python to the DB
@@ -67,7 +69,8 @@ def stations():
  #     """Return a list of stations."""
     stations = list(np.ravel(stations_df))
     return jsonify(stations=stations)
-  
+
+# Set up our route for the temperature observations(TOBS) data
 @app.route("/api/v1.0/tobs")
 def tobs():
     # Create our session (link) from Python to the DB
@@ -81,6 +84,30 @@ def tobs():
     temp_obs = list(np.ravel(tobs_df))
     # Return the results
     return jsonify(temps=temp_obs)
+
+# Set up our route allowing user to get back the min, avg, & max
+# --- temperature observation for their imputed date or date range
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    """Return TMIN, TAVG, TMAX."""
+    # Select statement
+    sel = [func.min(measurements.tobs), func.avg(
+        measurements.tobs), func.max(measurements.tobs)]
+    if not end:
+        # calculate TMIN, TAVG, TMAX for dates greater than start
+        results = session.query(*sel).\
+            filter(measurements.date >= start).all()
+    else:
+        # calculate TMIN, TAVG, TMAX with start and stop
+        results = session.query(*sel).\
+            filter(measurements.date >= start).\
+            filter(measurements.date <= end).all()
+    # Unravel results into a 1D array and convert to a list
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
 
 if __name__ == '__main__':
     app.run(debug=True)
